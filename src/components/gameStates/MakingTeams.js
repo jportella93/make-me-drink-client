891729d8b4fb2d71b57ff12f26afa7a2baab3fdd
jportella93 <jportella93@gmail.com>
2020-05-23 19:45:36 +0200

    feat: get team members

--- src/components/gameStates/MakingTeams.js
+++ src/components/gameStates/MakingTeams.js
@@ -1,15 +1,27 @@
 import React, { useContext } from 'react'
 import { RoomContext } from '../socketConnection'
-import { MAKING_TEAMS } from '../../constants/gameStates'
 
 const MakingTeams = () => {
   const {
-    users, userName, roomName, actions: { setGameState }
+    users, userName, teams
   } = useContext(RoomContext)
+
+  const getPartnerNames = () => {
+    const currentUserId = users.find(({ name }) => name === userName).id
+    const currentTeam = teams.find(team => team.includes(currentUserId))
+    const partnerArray = []
+    currentTeam.forEach(id => {
+      const user = users.find((user) => id === user.id && id !== currentUserId)
+      if (!user) return
+      partnerArray.push(user.name)
+    })
+    return partnerArray.join(', ')
+  }
 
   return (
     <>
-      Making teams!
+      <p>User: {userName}</p>
+      <p>You are teaming up with {getPartnerNames()}</p>
     </>
   )
 }
