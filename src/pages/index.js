a8094329ed3df745ad542ca74b799c556240c1ea
jportella93 <jportella93@gmail.com>
2020-05-23 12:27:02 +0200

    feat: remove default layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import Game from "../components/game"
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+import Game from '../components/game'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Game/>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
