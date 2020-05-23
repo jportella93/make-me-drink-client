9eb86ea0802e2c7585004b5355435858d72a8136
jportella93 <jportella93@gmail.com>
2020-05-23 19:45:25 +0200

    feat: minimum number of players

--- src/components/gameStates/WaitingRoom.js
+++ src/components/gameStates/WaitingRoom.js
@@ -2,36 +2,40 @@ import React, { useContext } from 'react'
 import { RoomContext } from '../socketConnection'
 import { MAKING_TEAMS } from '../../constants/gameStates'
 
+const MINIMUM_USER_NUMBER = 4
+
 const WaitingRoom = () => {
   const {
     users, userName, userType, roomName, actions: { setGameState }
   } = useContext(RoomContext)
 
-  return (
+  const usersLeftToPlay = MINIMUM_USER_NUMBER - users.length
+
+  return users && (
     <>
-      {userName && <h3>Your name: {userName}</h3>}
-      {users &&
-        <>
-        Online users in room {roomName}:
-          <ul>
-            {users.map(({ name, id, type }) => (
-              <li key={id}>
-                {name}{type === 'admin' && ' 👑'}
-              </li>
-            ))}
-          </ul>
-          {userType === 'admin'
-            ? (
-              <button onClick={() => setGameState(MAKING_TEAMS)}>
+      <p>Hi there, {userName}</p>
+      <p>Online users in room {roomName}:</p>
+      <ul>
+        {users.map(({ name, id, type }) => (
+          <li key={id}>
+            {name}{type === 'admin' && ' 👑'}
+          </li>
+        ))}
+      </ul>
+      {usersLeftToPlay > 0
+        ? <p>{usersLeftToPlay} more players needed</p>
+        : userType === 'admin'
+          ? (
+            <button
+              disabled={usersLeftToPlay > 0}
+              onClick={() => setGameState(MAKING_TEAMS)}>
               Start
-              </button>
-            )
-            : <p>
+            </button>
+          )
+          : <p>
             Waiting for {users.find(({ type }) => type === 'admin').name}{' '}
             to start the game
-            </p>
-          }
-        </>
+          </p>
       }
     </>
   )
