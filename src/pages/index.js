9fa0a0819c0f942260eb2c12c6e7423f25895645
jportella93 <jportella93@gmail.com>
2020-05-24 20:47:44 +0200

    feat: add gromet theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,25 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import SocketConnection from '../components/socketConnection'
+import { Grommet } from 'grommet'
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px'
+    }
+  }
+}
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <SocketConnection />
-  </Layout>
+  <Grommet theme={theme}>
+    <Layout>
+      <SEO title="Home" />
+      <SocketConnection />
+    </Layout>
+  </Grommet>
 )
 
 export default IndexPage
