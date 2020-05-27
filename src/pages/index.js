2c1031937d9281370500523e71c786b1c5e5240a
jportella93 <jportella93@gmail.com>
2020-05-27 21:28:01 +0200

    feat: some responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,18 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import SocketConnection from '../components/socketConnection'
+import { Box } from 'grommet'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <SocketConnection />
+    <Box
+      width={{ max: '500px', min: '50vw' }}
+      alignSelf="center"
+      alignContent="stretch"
+    >
+      <SocketConnection />
+    </Box>
   </Layout>
 )
 
