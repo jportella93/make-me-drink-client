4795a4a97d8b0d6d159f4f862a5b2a9d1c519411
Iddify <meedonbox@gmail.com>
2020-10-08 11:09:29 +0000

    undoing the formating

--- src/components/selectRoom.js
+++ src/components/selectRoom.js
@@ -1,55 +1,55 @@
-import React, { useContext } from "react";
-import { RoomContext } from "./socketConnection";
-import { Box, Form, FormField, TextInput, Button, Heading } from "grommet";
-import Spinner from "./Spinner";
+import React, { useContext } from 'react'
+import { RoomContext } from './socketConnection'
+import { Box, Form, FormField, TextInput, Button, Heading } from 'grommet'
+import Spinner from './Spinner'
 
 const SelectRoom = () => {
   const {
     isLoading,
     actions: { connectToRoom },
-  } = useContext(RoomContext);
+  } = useContext(RoomContext)
 
   function onSubmit(e) {
-    e.preventDefault();
-    const { roomName, userName } = e.target.elements;
-    connectToRoom(roomName.value, userName.value);
+    e.preventDefault()
+    const { roomName, userName } = e.target.elements
+    connectToRoom(roomName.value, userName.value)
   }
 
   return (
     <>
-      <Heading alignSelf="center" margin={{ bottom: "xlarge" }}>
+      <Heading alignSelf='center' margin={{ bottom: 'xlarge' }}>
         Make me Drink! 🍺
       </Heading>
       <Form onSubmit={onSubmit}>
         <FormField
           required
-          name="roomName"
-          htmlfor="roomName"
-          label="Room name"
+          name='roomName'
+          htmlfor='roomName'
+          label='Room name'
         >
-          <TextInput autoFocus id="roomName" name="roomName" />
+          <TextInput autoFocus id='roomName' name='roomName' />
         </FormField>
         <FormField
-          margin={{ top: "large" }}
+          margin={{ top: 'large' }}
           required
-          name="userName"
-          htmlfor="userName"
-          label="User name"
+          name='userName'
+          htmlfor='userName'
+          label='User name'
         >
-          <TextInput id="userName" name="userName" />
+          <TextInput id='userName' name='userName' />
         </FormField>
-        <Box pad={{ top: "xlarge" }} align="end">
+        <Box pad={{ top: 'xlarge' }} align='end'>
           <Button
             icon={isLoading ? <Spinner /> : null}
-            pad={{ top: "large" }}
-            type="submit"
+            pad={{ top: 'large' }}
+            type='submit'
             primary
-            label={isLoading ? "Loading" : "Submit"}
+            label={isLoading ? 'Loading' : 'Submit'}
           />
         </Box>
       </Form>
     </>
-  );
-};
+  )
+}
 
-export default SelectRoom;
+export default SelectRoom
