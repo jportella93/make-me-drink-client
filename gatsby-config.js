4870859d4ca4666371fd12e0e415bfa85ec319cf
Ahmad Reza <hungry.dev39@gmail.com>
2020-10-11 22:56:56 +0700

    add styled component plugin to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,9 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
       }
+    },
+    {
+      resolve: 'gatsby-plugin-styled-components'
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
